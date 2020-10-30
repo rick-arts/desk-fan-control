a780afda21b5ed779736c04f33bf0c7a41f2b32f
Rick Arts <rick@roefja.com>
2020-10-30 21:56:55 +0100

    Added ramp speed holders

--- desk_fan_control.ino
+++ desk_fan_control.ino
@@ -4,14 +4,17 @@
 const word PWM_FREQ_HZ = 25000;
 const word TCNT1_TOP = 16000000 / (2 * PWM_FREQ_HZ);
 
-#define LOW_TEMP 22
-#define HIGH_TEMP 30
+#define LOW_TEMP 21.5
+#define RAMP_TEMP 22
+#define HIGH_TEMP 28
 #define OFF_FRONT_TEMP 15
-#define OFF_BACK_TEMP 15
+#define OFF_BACK_TEMP 10
+#define BACK_ON_OFFSET 2
 
 #define EMERGENCY_SPEED 0
 #define FULLSPEED_SPEED 100
-#define LOW_SPEED 30
+#define LOW_SPEED 35
+#define RAMP_SPEED 50
 
 #define ONE_WIRE_BUS 7
 
@@ -22,6 +25,9 @@ const word TCNT1_TOP = 16000000 / (2 * PWM_FREQ_HZ);
 
 boolean emergency_mode = false;
 boolean full_speed_mode = false;
+boolean fan_front_off = false;
+boolean fan_back_off = false;
+boolean ramped = true;
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
@@ -57,24 +63,35 @@ void loop() {
   attachInterrupt(digitalPinToInterrupt(FULLSPEED_PIN), switchFullSpeed, RISING);
   digitalWrite(EMERGENCY_LED, LOW);
 
+  float temperature = measureTemp();
+  Serial.print("Temperature: ");
+  Serial.println(temperature);
+
   if (emergency_mode) {
     setFanspeed(EMERGENCY_SPEED);
     delay(250);
     digitalWrite(EMERGENCY_LED, HIGH);
     delay(250);
+    Serial.println("EMERGENCY STOP");
+    Serial.println();
   }
   else  {
-    float temperature = measureTemp();
-    Serial.print("Temperature: ");
-    Serial.println(temperature);
-
     front_fanspeed = getFanSpeed(temperature, "front");
     back_fanspeed = getFanSpeed(temperature, "back");
 
+    Serial.print("Speed front: ");
+    Serial.print(front_fanspeed);
+    Serial.print(" Speed back: ");
+    Serial.println(back_fanspeed);
+
+    Serial.print("Ramped: ");
+    Serial.println(ramped);
+
     setFanspeed(front_fanspeed, back_fanspeed);
     Serial.println();
-    delay(5000);
+    delay((temperature > RAMP_TEMP ? 10000 : 20000));
   }
+  
 }
 
 void setFanspeedFront(byte duty) {
@@ -90,27 +107,45 @@ void setFanspeed(int fan_speed) {
 }
 
 void setFanspeed(int speed_front, int speed_back) {
-  Serial.print("Speed front: ");
-  Serial.print(speed_front);
-  Serial.print(" Speed back: ");
-  Serial.println(speed_back);
-
   setFanspeedFront(speed_front);
   setFanspeedBack(speed_back);
 }
 
 int getFanSpeed(float temperature, String position) {
   int fanspeed = LOW_SPEED;
-  int OFF_TEMP = (position == "front" ? OFF_FRONT_TEMP : OFF_BACK_TEMP);
+  int OFF_TEMP = (position == "front" ? OFF_FRONT_TEMP : OFF_BACK_TEMP);  
+  if(ramped && temperature < LOW_TEMP) ramped = false;
 
-  if (temperature > HIGH_TEMP || full_speed_mode) {
+  if (full_speed_mode) return 100;
+
+  if (temperature > HIGH_TEMP) {
     fanspeed = 100;
   }
-  else if (temperature > LOW_TEMP) {
-    fanspeed += ceil(((100 - LOW_SPEED) / (HIGH_TEMP - LOW_TEMP)) * (temperature - LOW_TEMP));
+  else if (temperature >= RAMP_TEMP || ramped) {
+    fanspeed = RAMP_SPEED;
+    fanspeed += ceil(((100 - RAMP_SPEED) / (HIGH_TEMP - RAMP_TEMP)) * (temperature - RAMP_TEMP));
+    ramped = true;
+
+    if(fanspeed < RAMP_SPEED) fanspeed = RAMP_SPEED;
   }
-  else if (temperature < OFF_TEMP) {
+
+  if (temperature < OFF_TEMP) {
     fanspeed = 0;
+    if (position == "front") fan_front_off = true;
+    else fan_back_off = true;
+  }
+
+  if (position == "front")  {
+    if (fan_front_off && temperature < (OFF_TEMP + BACK_ON_OFFSET)) {
+      fanspeed = 0;
+    }
+    else fan_front_off = false;
+  }
+  else if (position == "back")  {
+    if (fan_back_off && temperature < (OFF_TEMP + BACK_ON_OFFSET)) {
+      fanspeed = 0;
+    }
+    else fan_back_off = false;
   }
 
   if (fanspeed > 100) fanspeed = 100;
@@ -121,7 +156,7 @@ int getFanSpeed(float temperature, String position) {
 
 float measureTemp() {
   sensors.requestTemperatures();
-  return ceil(sensors.getTempCByIndex(0));
+  return sensors.getTempCByIndex(0);
 }
 
 void switchEmergency() {
